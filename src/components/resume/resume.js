aa200c5613a3b89975d08f8106f38e295a49468c
Akash <sinha.aka@husky.neu.edu>
2021-04-07 02:57:17 -0400

    Added work experience

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,112 +1,114 @@
-import React, { Component } from 'react';
-export default  class Resume extends Component {
+import React, { Component } from "react";
+export default class Resume extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
       <section id="resume">
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1>
+              <span>Education</span>
+            </h1>
+          </div>
 
-         <div className="row education">
-
-            <div className="three columns header-col">
-               <h1><span>Education</span></h1>
-            </div>
-
-            <div className="nine columns main-col">
-              {
-                resumeData.education && resumeData.education.map((item)=>{
-                  return(
-                    <div className="row item">
-                       <div className="twelve columns">
-                          <h3>{item.UniversityName}</h3>
-                          <p className="info">
-                          {item.specialization}
-                          <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
-                          <p>
-                          {item.Achievements}
-                          </p>
-                       </div>
+          <div className="nine columns main-col">
+            {resumeData.education &&
+              resumeData.education.map((item) => {
+                return (
+                  <div className="row item">
+                    <div className="twelve columns">
+                      <h3>{item.UniversityName}</h3>
+                      <p className="info">
+                        {item.specialization}
+                        <span>&bull;</span>{" "}
+                        <em className="date">
+                          {item.MonthOfPassing} {item.YearOfPassing}
+                        </em>
+                      </p>
+                      <p>{item.Achievements}</p>
                     </div>
-                  )
-                })
-              }
-            </div>
-         </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
         <div className="row work">
-            <div className="three columns header-col">
-               <h1><span>Work</span></h1>
-            </div>
+          <div className="three columns header-col">
+            <h1>
+              <span>Work</span>
+            </h1>
+          </div>
 
-            <div className="nine columns main-col">
-              {
-                resumeData.work && resumeData.work.map((item) => {
-                  return(
-                    <div className="row item">
-                       <div className="twelve columns">
-                          <h3>{item.CompanyName}</h3>
-                          <p className="info">
-                          {item.specialization}
-                          <span>&bull;</span> <em className="date">{item.MonthOfLeaving} {item.YearOfLeaving}</em></p>
-                          <p>
-                          {item.Achievements}
-                          
-                          </p>
-                       </div>
-
+          <div className="nine columns main-col">
+            {resumeData.work &&
+              resumeData.work.map((item) => {
+                return (
+                  <div className="row item">
+                    <div className="twelve columns">
+                      <h3>{item.CompanyName}</h3>
+                      <p className="info">
+                        {item.specialization}
+                        <span>&bull;</span>{" "}
+                        <em className="date">
+                          {item.MonthOfLeaving} {item.YearOfLeaving}
+                        </em>
+                      </p>
+                      <p>{item.Achievements}</p>
                     </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
 
-                  )
-                })
-              }
-            </div> 
-         </div>
+        <div className="row skill">
+          <div className="three columns header-col">
+            <h1>
+              <span>Skills</span>
+            </h1>
+          </div>
 
+          <div className="nine columns main-col">
+            <p>
+              <b>{resumeData.skillsDescription}</b>
+              <br />
+              <span className=" display">
+                <b>Programming Languages:</b>
+                JavaScript (ES6), Java, Python, SQL <br />
+                <b>Web Technologies: </b>
+                HTML, CSS, React, D3.js, Angular, Redux, Node.js, REST APIs,
+                Spring MVC/Boot, JSP
+                <br />
+                <b>Database: </b>
+                PostgreSQL, MongoDB, MySQL, SAP HANA, Oracle
+                <br />
+                <b>Software Tools: </b>
+                Visual Studio Code, IntelliJ, Apache Tomcat, Eclipse, Anaconda,
+                NetBeans, Tableau, Postman
+                <br />
+                <b>Other Tools: </b>
+                AWS, Docker, GitHub, Jira, Confluence, Bitbucket, Figma
+                <br />
+              </span>
+            </p>
 
-         <div className="row skill">
-
-            <div className="three columns header-col">
-               <h1><span>Skills</span></h1>
-            </div>
-
-            <div className="nine columns main-col">
-
-               <p>
-               <b>{resumeData.skillsDescription}</b><br />
-               <span className = " display">
-               <b>Programming Languages:</b>
-               Java, Python, SQL <br />
-               <b>Web Technologies: </b> 
-               Angular, React, JavaScript, JSX, Spring MVC, Hibernate, Node.js, HTML5, SCSS, Swing, Servlets<br />
-               <b>Version Control: </b> 
-               GitHub, Bitbucket<br />
-               <b>Software Tools: </b> 
-               Visual Studio Code, IntelliJ, Apache Tomcat, Eclipse, Anaconda, NetBeans, Tableau, Postman<br />
-               <b>Database: </b> 
-               MongoDB, SAP HANA, Oracle, MySQL<br />
-               </span>
-               </p>
-
-   				<div className="bars">
-
-   				   <ul className="skills">
-                {
-                  resumeData.skills && resumeData.skills.map((item) => {
-                    return(
+            <div className="bars">
+              <ul className="skills">
+                {resumeData.skills &&
+                  resumeData.skills.map((item) => {
+                    return (
                       <li>
-                      <span className={`bar-expand ${item.skillname.toLowerCase()}`}>
-                      </span><em>{item.skillname}</em>
+                        <span
+                          className={`bar-expand ${item.skillname.toLowerCase()}`}
+                        ></span>
+                        <em>{item.skillname}</em>
                       </li>
-                    )
-                  })
-                }
-
-   					</ul>
-
-   				</div>
-
-   			</div>
-
-         </div>
-
+                    );
+                  })}
+              </ul>
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
